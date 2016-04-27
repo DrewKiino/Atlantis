4c84464d23feda7c821125f7c26bf2a585e658a6
Andrew Aquino <andrewaquino118@yahoo.com>
2016-04-26 19:03:22 -0700

    added license

---
+++ XCode/Atlantis/Pods/Target Support Files/Atlantis/Atlantis-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Atlantis : NSObject
+@end
+@implementation PodsDummy_Atlantis
+@end
---
+++ XCode/Atlantis/Pods/Target Support Files/Atlantis/Atlantis-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AtlantisVersionNumber;
+FOUNDATION_EXPORT const unsigned char AtlantisVersionString[];
+
