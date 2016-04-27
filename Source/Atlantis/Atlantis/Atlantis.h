dc686d5e1c21f008d13ceb24eb5631ebfd91d84a
Andrew Aquino <andrewaquino118@yahoo.com>
2016-04-26 18:34:59 -0700

    updating atlantis

---
+++ Source/Atlantis/Atlantis/Atlantis.h
@@ -0,0 +1,19 @@
+//
+//  Atlantis.h
+//  Atlantis
+//
+//  Created by Andrew Aquino on 4/26/16.
+//  Copyright © 2016 Andrew Aquino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Atlantis.
+FOUNDATION_EXPORT double AtlantisVersionNumber;
+
+//! Project version string for Atlantis.
+FOUNDATION_EXPORT const unsigned char AtlantisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Atlantis/PublicHeader.h>
+
+
